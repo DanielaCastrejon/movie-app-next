9b1de16e674150e6986e5029f4f5a36046bb1ec7
DanielaCastrejon <danielakastrejon@gmail.com>
2024-10-14 15:09:17 -0600

    Se agrega la barra de búsqueda y el filtrado de resultados de la búsqueda, ya se pueden ver las imagenes y se hizo una restructuración en el src

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,8 +1,9 @@
 import localFont from "next/font/local";
 import "./globals.css";
-import Header from "./components/Header";
+import Header from "../components/Header";
 import Providers from "./Providers";
-import Navbar from "./components/Navbar";
+import Navbar from "../components/Navbar";
+import SearchBox from "../components/SearchBox";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -29,6 +30,7 @@ export default function RootLayout({ children }) {
         <Providers>
             <Header/>
             <Navbar/>
+            <SearchBox/>
             {children}
         </Providers>
       </body>
